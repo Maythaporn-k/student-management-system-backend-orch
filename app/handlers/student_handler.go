5369c0d2f2ed07e9c8aa644cb7a258030e38ed0c
Maythaporn <maythaporn.k@arise.tech>
2025-01-27 10:27:58 +0700

    update core path

--- app/handlers/student_handler.go
+++ app/handlers/student_handler.go
@@ -7,7 +7,7 @@ import (
 )
 
 // const coreBaseURL = "http://localhost:8002" // Core service URL
-const coreBaseURL = "https://student-management-448804.el.r.appspot.com/" // Core service URL
+const coreBaseURL = "http://student-management-system-backend-core.student-management-448804.el.r.appspot.com/" // Core service URL
 
 func sendRequestToCoreValidate(method, endpoint string, body io.Reader) (*http.Response, error) {
 	url := fmt.Sprintf("%s%s", coreBaseURL, endpoint)
